ebc0d44b08f210901b7c36e8a66a331f62fae4a7
exploit-j <kkarismajh@gmail.com>
2022-04-04 17:26:16 +0900

    click to scroll 기능 적용

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ const overlay = document.querySelector('.overlay')
 
 sideMenuButton.addEventListener('click', () =>
   sideMenu.classList.toggle('is-active'))
+
+const moveButton = document.querySelectorAll('.nav-button')
+const topHeight = 50
+
+function clicked(){
+  const sectionId = this.getAttribute('id')
+  const toSection = document.querySelector(`.${sectionId}`)
+  console.log(sectionId);
+  console.log(toSection);
+
+  const scrollAmount = toSection.offsetTop - topHeight
+  console.log(scrollAmount)
+
+  window.scrollTo({
+    top : scrollAmount,
+    behavior : 'smooth'
+  })
+
+  sideMenu.classList.remove('is-active')
+}
+
+moveButton.forEach((button) => {
+  button.addEventListener('click', clicked)
+})
