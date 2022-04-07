0111bf5548c062ad64d363c305a4e46052a8cc9e
exploit-j <kkarismajh@gmail.com>
2022-04-07 16:22:01 +0900

    공통사용 form 제작

---
+++ js/home&side-menu.js
@@ -0,0 +1,26 @@
+// 공통기능 (home, sidemenu버튼, underline)
+
+const sideMenuButton = document.querySelector('.side-menu-button')
+const sideMenu = document.querySelector('nav')
+const homeButton = document.querySelector('header .logo')
+const listItem = document.querySelectorAll('header nav li')
+sideMenuButton.addEventListener('click', () =>
+  sideMenu.classList.toggle('is-active'))
+
+homeButton.addEventListener('click', () => {
+  window.scrollTo({
+    top : 0, 
+    behavior : 'smooth'
+  })
+  sideMenu.classList.remove('is-active')
+})
+
+const underline = document.querySelector('.underline')
+function underlineEffect(e){
+  underline.classList.add('is-active')
+  underline.style.left = e.currentTarget.offsetLeft + 'px';
+  underline.style.width = e.currentTarget.offsetWidth + 'px';
+  underline.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight - 3 + 'px';
+}
+
+listItem.forEach(item => item.addEventListener('click', (e) => underlineEffect(e)))
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const sideMenuButton = document.querySelector('.side-menu-button')
 const sideMenu = document.querySelector('nav')
-const overlay = document.querySelector('.overlay')
 
 sideMenuButton.addEventListener('click', () =>
   sideMenu.classList.toggle('is-active'))
